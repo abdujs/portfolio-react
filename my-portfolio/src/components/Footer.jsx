3604cdb885732dd35945c7cb5cc627060a7158db
abdujs <abdu12.aau@gmail.com>
2025-02-03 00:09:54 +0300

    Update Footer component with social media links and improved styling

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,23 +1,23 @@
 // src/components/Footer.jsx
-import React from 'react';
+import {FaLinkedin, FaGithub, FaTelegram, FaEnvelope} from 'react-icons/fa'
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4">
+    <footer className="bg-gray-700 text-white p-4">
       <div className="container mx-auto text-center">
         <h2 className="text-xl font-bold mb-4">Connect with me</h2>
         <div className="flex justify-center space-x-4 mb-4">
-          <a href="#" className="hover:text-blue-500">
-            {/* LinkedIn Icon */}
+          <a href="https://t.me/Abdulwahid_Munewer" className="hover:text-blue-400">
+            <FaTelegram size={22} style={{ color: '#FFFFFF' }} />
           </a>
-          <a href="#" className="hover:text-gray-400">
-            {/* GitHub Icon */}
+          <a href="https://github.com/Abdujs" className="hover:text-gray-400">
+            <FaGithub size={22} style={{ color: '#FFFFFF' }} />
           </a>
-          <a href="#" className="hover:text-blue-400">
-            {/* Twitter Icon */}
+          <a href="https://www.linkedin.com/in/abdulwahid-m/" className="hover:text-blue-500">
+            <FaLinkedin size={22} style={{ color: '#FFFFFF' }} />
           </a>
-          <a href="#" className="hover:text-pink-500">
-            {/* Instagram Icon */}
+          <a href="mailto:a.wahid.developer@gmail.com" className="hover:text-red-500">
+            <FaEnvelope size={22} style={{ color: '#FFFFFF' }} />
           </a>
         </div>
         <p>&copy; 2025 Abdulwahid Munewer. All rights reserved.</p>
